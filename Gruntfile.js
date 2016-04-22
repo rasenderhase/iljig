4fbed8dd770044bacc76428e00794bfebc0fa19d
Andreas Knees <andreas.knees@nikem.de>
2016-04-23 00:55:26 +0200

    code coverage

--- Gruntfile.js
+++ Gruntfile.js
@@ -64,5 +64,5 @@ module.exports = function(grunt) {
 
     // Default task(s).
     grunt.registerTask('default', ['mochaTest']);
-
+    grunt.registerTask('travisci', ['mocha_istanbul', 'codacy']);
 };
