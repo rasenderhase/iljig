2a3b6c37cfdbecb247445a6cee9ff34b62d1d1ec
Andreas Knees <andreas.knees@nikem.de>
2016-06-16 01:01:35 +0200

    add tests

--- test/modules/iljig/DBService_test.js
+++ test/modules/iljig/DBService_test.js
@@ -1,5 +1,8 @@
-var should, db, s;
+var should, log4js, logger, db, s;
+
 should = require("should");
+log4js = require('log4js');
+logger = log4js.getLogger("iljig.DbService_test");
 
 db = require("../../../modules/iljig/DBService.js");
 s = require("../../../modules/iljig/SpielIljig.js");
@@ -12,11 +15,35 @@ describe("DBService", function() {
     });
 
     describe("#saveSpiel", function() {
-        it("should be a promise", function() {
+        it("should save a spiel", function() {
             dbService.saveSpiel(spiel).should.be.a.Promise();
             dbService.db.spiel.should.have.ownProperty(4711);
             dbService.db.spiel[4711].should.be.eql(spiel.toDb());
             dbService.db.spiel[4711]["adminGeheimnis"].should.be.equal("geheim", "falsches adminGeheimnis");
+        });
+    });
+
+    describe("#getSpiel", function() {
+        it("should get the saved spiel with different 'lastAcces' date", function() {
+            var getSpielPromise;
+            dbService.saveSpiel(spiel);
+            getSpielPromise = dbService.getSpiel(4711);
+
+            getSpielPromise.done(function(res, err) {
+                if (err) {
+                    logger.debug(err);
+                }
+                else {
+                    logger.debug(res.toDb());
+                    logger.debug(spiel.toDb());
+                }
+            });
+
+            // Property löschen, da unterschiedlich
+            delete spiel.lastAccess;
+            // bei Promises muss das Assertion-Objekt zurückgegeben werden
+            // https://shouldjs.github.io/#assertion-finally
+            return getSpielPromise.should.finally.have.properties(spiel);
         });
     });
 });
