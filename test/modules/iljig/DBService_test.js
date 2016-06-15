e35fa7a5ce5a479952b68c0b6529e2f55c5a8c18
Andreas Knees <andreas.knees@nikem.de>
2016-06-15 22:44:51 +0200

    add tests

--- test/modules/iljig/DBService_test.js
+++ test/modules/iljig/DBService_test.js
@@ -1,10 +1,22 @@
-var should, db;
+var should, db, s;
 should = require("should");
 
 db = require("../../../modules/iljig/DBService.js");
+s = require("../../../modules/iljig/SpielIljig.js");
 
 describe("DBService", function() {
+    var dbService = db.dbService;
+    var spiel;
+    beforeEach(function() {
+        spiel = new s.SpielIljig(4711, "geheim");
+    });
+
     describe("#saveSpiel", function() {
-        it("should save a spiel");
+        it("should be a promise", function() {
+            dbService.saveSpiel(spiel).should.be.a.Promise();
+            dbService.db.spiel.should.have.ownProperty(4711);
+            dbService.db.spiel[4711].should.be.eql(spiel.toDb());
+            dbService.db.spiel[4711]["adminGeheimnis"].should.be.equal("geheim", "falsches adminGeheimnis");
+        });
     });
 });
